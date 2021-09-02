08ae101314583349a9cd37f530c0a11fc27a276c
SrimanAbhinauv <78959409+SrimanAbhinauv@users.noreply.github.com>
2021-09-02 17:08:32 +0530

    Add files via upload

---
+++ Zombie-Shooter-stage-1-main/sketch.js
@@ -0,0 +1,85 @@
+var bg,bgImg;
+var player, shooterImg, shooter_shooting;
+var zombie,zombieImg,zombieGroup
+
+function preload(){
+
+  zombieImg = loadImage("assets/zombie.png")
+  shooterImg = loadImage("assets/shooter_2.png")
+  shooter_shooting = loadImage("assets/shooter_3.png")
+
+  bgImg = loadImage("assets/bg.jpeg")
+
+}
+
+function setup() {
+  cloudsGroup = createGroup();
+  
+  createCanvas(windowWidth,windowHeight);
+
+  //adding the background image
+  bg = createSprite(displayWidth/2-20,displayHeight/2-40,20,20)
+bg.addImage(bgImg)
+bg.scale = 1.1
+  
+
+//creating the player sprite
+player = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
+ player.addImage(shooterImg)
+   player.scale = 0.3
+   player.debug = true
+   player.setCollider("rectangle",0,0,300,300)
+
+
+}
+
+function draw() {
+  background(0); 
+
+
+  zombieGroup.setLifetimeEach(-1);
+  
+
+  //moving the player up and down and making the game mobile compatible using touches
+if(keyDown("UP_ARROW")||touches.length>0){
+  player.y = player.y-30
+}
+if(keyDown("DOWN_ARROW")||touches.length>0){
+ player.y = player.y+30
+}
+
+
+//release bullets and change the image of shooter to shooting position when space is pressed
+if(keyWentDown("space")){
+ 
+  player.addImage(shooter_shooting)
+ 
+}
+
+//player goes back to original standing image once we stop pressing the space bar
+else if(keyWentUp("space")){
+  player.addImage(shooterImg)
+}
+spawnZombie();
+drawSprites();
+
+}
+function spawnZombie() {
+  //write code here to spawn the clouds
+  if (frameCount % 60 === 0) {
+    zombie = createSprite(displayWidth-300,displayHeight-300,50,50);
+zombie.addImage(zombieImg)
+zombie.scale = 0.2
+    zombie.velocityX = -3;
+    
+     //assign lifetime to the variable
+    zombie.lifetime = 200;
+    
+    //adjust the depth
+    zombie.depth =player.depth;
+    player.depth = player.depth + 1;
+    
+    //add each cloud to the group
+   // zombieGroup.add(zombie);
+  }
+}
